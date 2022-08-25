c7c522bdfb0bb811b9619cf49d04b271d53cc450
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-08-25 07:51:52 +0530

    display O,X on screen alternatively

--- index.js
+++ index.js
@@ -1,15 +1,77 @@
-const gameBoard = () => {
-  let board = ["", "", "", "", "", "", ""];
+const Player = (sign) => {
+  const symbol = sign;
+
+  const getSymbol = () => {
+    return symbol;
+  };
+
+  return { getSymbol };
 };
 
-function updateCell(e) {
-  e.target.textContent = "X";
-}
+const gameBoard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
 
-document
-  .querySelectorAll(".grid-item")
-  .forEach((item) => item.addEventListener("click", (e) => updateCell(e)));
+  const setGridCell = (index, symbol) => {
+    if (index > board.length) return;
+    board[index] = symbol;
+    console.log("ray", board);
+  };
+
+  const getGridCell = (index) => {
+    if (index > board.length) return;
+    return board[index];
+  };
+
+  const reset = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
+    }
+  };
+
+  return { reset, setGridCell, getGridCell };
+})();
+
+// function updateCell(e) {
+//   e.target.textContent = "X";
+//   console.log("why", e.target.dataset.index);
+// }
+
+const gameController = (() => {
+  let playerX = Player("X");
+  let playerO = Player("O");
+  let round = 1;
+
+  const playRound = (index) => {
+    gameBoard.setGridCell(index, getPlayerSymbol());
+    round++;
+  };
+
+  const getPlayerSymbol = () =>
+    round % 2 === 1 ? playerX.getSymbol() : playerO.getSymbol();
+
+  return {
+    playRound,
+  };
+})();
+
+const displayController = (() => {
+  document.querySelectorAll(".grid-item").forEach((item) => {
+    gameController.playRound(item.dataset.index);
+    let symbol = gameBoard.getGridCell(item.dataset.index);
+    item.addEventListener("click", (e) => setCellDisplay(e, symbol));
+  });
+
+  function setCellDisplay(e, symbol) {
+    e.target.textContent = symbol;
+    console.log("whoop");
+  }
+})();
 
 // document
-//   .getElementById("woof")
-//   .addEventListener("click", (e) => (e.target.textContent = "ewd"));
+//   .querySelectorAll(".grid-item")
+//   .forEach((item) =>
+//     item.addEventListener(
+//       "click",
+//       gameBoard.setGridCell(e.target.dataset.index, "X")
+//     )
+//   );
